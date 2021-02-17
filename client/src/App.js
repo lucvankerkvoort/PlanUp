e4c35666470f4c1e32eeb1624e27401f3ff98d92
joydeep singha <joydeepsingha68@gmail.com>
2021-02-17 17:47:28 +0530

    Full calender added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calender from "./Components/Calender/Calender"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Calender/>
+    </div>
+  );
+}
+
+export default App;
